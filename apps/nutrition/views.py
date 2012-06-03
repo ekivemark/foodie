b18e195c285fc71bff8bdb3f6189038d339dce58
Alan Viars <aviars@videntity.com>
2012-06-03 14:21:48 -0400

    foo

--- apps/nutrition/views.py
+++ apps/nutrition/views.py
@@ -4,27 +4,27 @@ from django.conf import settings
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from models import *
-def loaddata(request):
-    csvfile = csv.reader(open('/home/alan/django-projects/foodie/data/nutrition3.csv', 'rb'), delimiter=',')
-    rowindex = 0
-    for row in csvfile:
-        print row
-
-        doc={}
-        sitem=row[0].split(',')
-        
-        print sitem
-        
-        doc['place'] =sitem[0]
-        print sitem
-        
-        if len(sitem)>1:
-            doc['item'] =sitem[1]
-            doc['calories']=row[1]
-            Nutrition.objects.create(place=doc['place'], item=doc['item'],
-                                 calories=doc['calories'])
-            
-    print "done."
+#def loaddata(request):
+#    csvfile = csv.reader(open('/home/alan/django-projects/foodie/data/nutrition3.csv', 'rb'), delimiter=',')
+#    rowindex = 0
+#    for row in csvfile:
+#        print row
+#
+#        doc={}
+#        sitem=row[0].split(',')
+#        
+#        print sitem
+#        
+#        doc['place'] =sitem[0]
+#        print sitem
+#        
+#        if len(sitem)>1:
+#            doc['item'] =sitem[1]
+#            doc['calories']=row[1]
+#            Nutrition.objects.create(place=doc['place'], item=doc['item'],
+#                                 calories=doc['calories'])
+#            
+#    print "done."
 
 def suggest(request):
     if request.method == 'POST':
@@ -44,4 +44,5 @@ def suggest(request):
     
     return render_to_response('home/index.html', {'form':SuggestForm()},
                               RequestContext(request))
+
     
